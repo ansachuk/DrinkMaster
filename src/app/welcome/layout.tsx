b7b2f23e54406f9c6a516b9fad822562ff5d23e7
Andrii Sachuk <ansachuk@icloud.com>
2023-10-31 20:25:15 +0200

    add welcome layout

--- src/app/welcome/layout.tsx
+++ src/app/welcome/layout.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 import isLoggedIn from "../boo";
 import { redirect } from "next/navigation";
 
+import css from "./WelcomePage.module.scss";
+
 type Props = { children: React.ReactNode };
 
-export default function RootLayout({ children }: Props) {
+export default function Layout({ children }: Props) {
 	return isLoggedIn ? (
 		redirect("/")
 	) : (
-		<main>
-			welcome layout
+		<div className={css.containerLayout}>
+			<div className={css.gradientContainer}></div>
+			<div className={css.circleContainer}></div>
 			{children}
-			<Link href="/welcome/signin">signin</Link>
-			<br />
-			<Link href="/welcome/signup">signup</Link>
-		</main>
+		</div>
 	);
 }
