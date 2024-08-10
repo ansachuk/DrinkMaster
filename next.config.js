a1da1795dd29100d9a682db298487ee718c65d47
Andrii Sachuk <ansachuk@icloud.com>
2024-08-10 21:56:13 +0200

    stashed all the data

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const withImages = require("next-images");
-
-const nextConfig = {
-	withImages: withImages(),
-};
-
-module.exports = nextConfig;
--- src/app/(app)/drinks/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Drinks() {
-	return <div>Drinks</div>;
-}
